32d087fc819eecfe541b834b4f11b3166dd7dbab
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-28 19:12:27 -0500

    useMap custom hook created

--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -1,25 +1,7 @@
-import {useRef, useState, useEffect} from "react";
-import mapboxgl from "mapbox-gl"
-
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string;
+import { useMap } from "../../hooks/useMap";
 
 const Map = () => {
-	const mapContainer = useRef<HTMLDivElement | null>(null)
-	const map = useRef<Map<any, any> | null>(null)
-	const [lng, setLng] = useState(-70.9)
-	const [lat, setLat] = useState(42.35)
-	const [zoom, setZoom] = useState(9)
-
-	useEffect(() => {
-		if (map.current) return
-
-		map.current = new mapboxgl.Map({
-			container: mapContainer.current as HTMLDivElement,
-			style: 'mapbox://styles/mapbox/streets-v11',
-			center: [lng, lat],
-			zoom: zoom
-		})
-	})
+	const { mapContainer } = useMap()
 
 	return (
 		<div ref={mapContainer} className="w-full h-[100vh] overflow-hidden"></div>
---
+++ hooks/useMap.ts
@@ -0,0 +1,27 @@
+import {useRef, useState, useEffect} from "react";
+import mapboxgl from "mapbox-gl"
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string;
+
+export const useMap = () => {
+	const mapContainer = useRef<HTMLDivElement | null>(null)
+	const map = useRef<Map<any, any> | null>(null)
+	const [lng, setLng] = useState(-70.9)
+	const [lat, setLat] = useState(42.35)
+	const [zoom, setZoom] = useState(9)
+
+	useEffect(() => {
+		if (map.current) return
+
+		map.current = new mapboxgl.Map({
+			container: mapContainer.current as HTMLDivElement,
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [lng, lat],
+			zoom: zoom
+		})
+	})
+
+	return {
+		mapContainer
+	}
+}
