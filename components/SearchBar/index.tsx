5bf0955f7861dd1f53fac615df21b90092430769
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-14 13:42:33 -0600

    Display results in search bar

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -8,20 +8,32 @@ export const SearchBar = () => {
 	const { linesData, stations } = useContext(globalContext) as GlobalContext
 	const { handleChange, searchResult  } = useSearch({ linesData, stations })
 
-	console.log(searchResult)
-
 	return(
-		<form className="absolute top-4 left-[50%] translate-x-[-50%] py-2 px-3 bg-white rounded-lg w-[70%] flex items-center space-x-1">
-			<label htmlFor="search">
-				<AiOutlineSearch size={"1.3rem"} />
-			</label>
-			<input
-				onChange={handleChange}
-				type="text" name="search" id="search"
-				placeholder="Search..."
-				className="outline-none w-full text-lg"
-				autoComplete="off"
-			/>
-		</form>
+		<section className="modal-style flex-col">
+			<form className="input-style flex items-center space-x-1 top-4 mb-2">
+				<label htmlFor="search">
+					<AiOutlineSearch size={"1.3rem"} />
+				</label>
+				<input
+					onChange={handleChange}
+					type="text" name="search" id="search"
+					placeholder="Search..."
+					className="outline-none w-full text-lg"
+					autoComplete="off"
+				/>
+			</form>
+
+			{searchResult.length > 0 && (
+				<article className="top-16 input-style">
+					<ul>
+						{searchResult.map(result =>
+							<li key={result.id}>
+								{result.name}
+							</li>
+						)}
+					</ul>
+				</article>
+			)}
+		</section>
 	)
 }
