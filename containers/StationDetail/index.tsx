1d6adb6ff3537b099134e044c5ff0dea0b95c7fb
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-16 14:00:07 -0600

    Styling Station Detail container

--- containers/StationDetail/index.tsx
+++ containers/StationDetail/index.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react"
 import { IoIosArrowUp } from "react-icons/io"
 import { MdClear } from "react-icons/md"
 
 export const StationDetail = () => {
+	const [detailHeight, setDetailHeight] = useState("50px")
+	const handleDisplay = () => {
+		const nextValue = detailHeight === "50px" ? "70vh" : "50px"
+		setDetailHeight(nextValue)
+	}
+
 	return(
-		<section className="modal-style station-detail">
+		<section className="modal-style">
 			<header>
-				<button>
+				<button
+					onClick={handleDisplay}
+					className={detailHeight === "70vh" ? "arrow-down" : ""}
+				>
 					<IoIosArrowUp size="30" />
 				</button>
 				{/* <MdClear size="27" /> */}
@@ -17,6 +27,23 @@ export const StationDetail = () => {
 					justify-content: end;
 					padding: 2px;
 					border-bottom: 1px solid;
+				}
+
+				button {
+					transition: transform 1s 1s ease-out;
+				}
+
+				section {
+					transition: height 1s ease-out;
+					background-color: white;
+					border-radius: 10px 10px 0 0;
+					bottom: 0;
+					height: ${detailHeight};
+					width: 95%;
+				}
+
+				.arrow-down {
+					transform: rotate(180deg);
 				}
 			`}</style>
 		</section>
