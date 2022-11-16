dd9492ce2b4e05889fb558081f698b4414035896
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-16 13:08:23 -0600

    initial style added to StationDetail container

---
+++ containers/StationDetail/index.tsx
@@ -0,0 +1,5 @@
+export const StationDetail = () => {
+	return(
+		<div className="modal-style station-detail"></div>
+	)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react"
 import { Map } from "@app/components/Map"
+import { ConditionalNode } from "@app/components/ConditionalNode"
 import { SearchForm } from "@app/containers/SearchForm"
+import { StationDetail } from "@app/containers/StationDetail"
 import { GlobalProvider } from "@app/contexts"
 import { initialState } from "@app/contexts/initialState"
 import { useMap } from "@app/hooks/useMap"
 import { GlobalContext } from "@app/types"
 
 export default function Home() {
-	const [detailMode, setDetailMode] = useState(false)
+	const [detailMode, setDetailMode] = useState(true)
 	const toggleDetailMode = () => setDetailMode(prev => !prev)
 	const map = useMap()
 
@@ -21,7 +23,14 @@ export default function Home() {
   return (
 		<GlobalProvider value={globalInitValue}>
 			<main className="w-full h-[100vh] relative">
-				<SearchForm />
+				<ConditionalNode conditional={!detailMode}>
+					<SearchForm />
+				</ConditionalNode>
+
+				<ConditionalNode conditional={detailMode}>
+					<StationDetail />
+				</ConditionalNode>
+
 				<Map />
 			</main>
 		</GlobalProvider>
